701b43248c0f8995dd1fb9976c218785567f148c
Patrick Janssen <patrick@janssen.name>
2018-12-03 12:13:11 +0800

    Exported all modules, including beijing 8

--- src/typescript/_export.ts
+++ src/typescript/_export.ts
@@ -1,14 +1,14 @@
-// import * as calc from "./calc";
-// export {calc};
+import * as calc from "./calc";
+export {calc};
 
 import * as io from "./io";
 export {io};
 
-// import * as modify from "./modify";
-// export {modify};
+import * as modify from "./modify";
+export {modify};
 
-// import * as create from "./create";
-// export {create};
+import * as create from "./create";
+export {create};
 
 import * as beijing from "./beijingFns";
 export {beijing};
